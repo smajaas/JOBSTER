61d87e262e13693271c447792128e006342a140a
smajaas <smajaas@gmail.com>
2023-06-12 11:07:30 +0530

    import and set up styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import Landing from './pages/Landing';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 2rem;
+`;
 
 function App() {
   return (
     <div>
+      <Button>Click me</Button>
       <Landing />
     </div>
   );
