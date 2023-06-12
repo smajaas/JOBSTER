762da87d4ad5c2c053f1e3d2f7cec3fcfc56512f
smajaas <smajaas@gmail.com>
2023-06-12 11:46:31 +0530

    style lanfing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,67 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import styled from 'styled-components';
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt='jobster logo' className='logo' />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            750 million+ members | Manage your professional identity. Build and
+            engage with your professional network. Access knowledge, insights
+            and opportunities.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
 export default Landing;
