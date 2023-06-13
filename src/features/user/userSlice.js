45e75554646c3e0d15857e0838ad070587c0dfe1
smajaas <smajaas@gmail.com>
2023-06-13 23:43:33 +0530

    login user

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -21,7 +21,12 @@ export const registerUser = createAsyncThunk(
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
-    console.log(`Login User :${user}`);
+    try {
+      const resp = await customFetch.post('/auth/login', user);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
   }
 );
 
@@ -39,6 +44,19 @@ const userSlice = createSlice({
       toast.success(`Hello there ${user.name}`);
     },
     [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [loginUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      const { user } = payload;
+      state.isLoading = false;
+      state.user = user;
+      toast.success(`Welcome back ${user.name}`);
+    },
+    [loginUser.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
