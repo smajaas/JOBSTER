e25c0e3213048f000f1462a7095562ac35d4310d
smajaas <smajaas@gmail.com>
2023-06-14 20:57:36 +0530

    bigside bar setup complete

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,9 +1,24 @@
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <h2>BigSidebar</h2>;
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
