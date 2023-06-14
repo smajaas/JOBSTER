55fb6aacc0a4bf8f104017f22b2d1bc7162dd098
smajaas <smajaas@gmail.com>
2023-06-14 17:41:24 +0530

    logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Logo from './Logo';
 import { useState } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleSidebar } from '../features/user/userSlice';
+import { toggleSidebar, logoutUser } from '../features/user/userSlice';
 
 const Navbar = () => {
   const [showLogout, setShowLogout] = useState(false);
@@ -39,7 +39,7 @@ const Navbar = () => {
             <button
               type='button'
               className='dropdown-btn'
-              onClick={() => console.log('logout user')}
+              onClick={() => dispatch(logoutUser())}
             >
               logout
             </button>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -43,6 +43,11 @@ const userSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      removeFromLocalStorage();
+    },
   },
   extraReducers: {
     [registerUser.pending]: (state) => {
@@ -76,6 +81,6 @@ const userSlice = createSlice({
   },
 });
 
-export const { toggleSidebar } = userSlice.actions;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
 
 export default userSlice.reducer;
