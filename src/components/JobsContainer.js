b430f6fc1b84e57d50acfe6deb794dcf07f22dfc
smajaas <smajaas@gmail.com>
2023-06-16 20:50:03 +0530

    jobs container structure

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,4 +1,37 @@
+import { useEffect } from 'react';
+import Job from './Job';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import Loading from './Loading';
+
 const JobsContainer = () => {
-  return <h1>JobsContainer</h1>;
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <Loading center />
+      </Wrapper>
+    );
+  }
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <h5>job info</h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          console.log(job);
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 export default JobsContainer;
