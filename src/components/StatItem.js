3969754dc7cc8bbb33670903bc9ec0cb8f328615
smajaas <smajaas@gmail.com>
2023-06-19 11:50:32 +0530

    charts container

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,4 +1,14 @@
-const StatItem = () => {
-  return <h1>StatItem</h1>;
+import Wrapper from '../assets/wrappers/StatItem';
+
+const StatItem = ({ count, title, bcg, icon, color }) => {
+  return (
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
+  );
 };
 export default StatItem;
