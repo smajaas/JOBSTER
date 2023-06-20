64029344e281322ba0f2ad37c7f1e88745d84eb7
smajaas <smajaas@gmail.com>
2023-06-20 20:57:13 +0530

    job js bug

--- src/components/Job.js
+++ src/components/Job.js
@@ -30,14 +30,12 @@ const Job = ({
       </header>
       <div className='content'>
         <div className='content-center'>
-          <div className='content-center'>
-            <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
-            <JobInfo icon={<FaCalendarAlt />} text={date} />
-            <JobInfo icon={<FaBriefcase />} text={jobType} />
-            <div className={`status ${status}`}>{status}</div>
-          </div>
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
           <div className={`status ${status}`}>{status}</div>
         </div>
+
         <footer>
           <div className='actions'>
             <Link
